f3b98f898a59aa1f96239a259febe6fc8471a173
Sándor Szalárdy <szalardy@c3d.hu>
2018-12-01 13:06:35 +0000

    Prevent Jekyll build

---
+++ bundle.js
@@ -0,0 +1,62 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+let Phrase = require("szalars-palindrome");
+let string = prompt("Please enter a string for palindrome testing");
+
+let phrase = new Phrase(string);
+if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome! :)`);
+} else {
+    alert(`"${phrase.content}" is not a palindrome. :(`);
+}
+},{"szalars-palindrome":2}],2:[function(require,module,exports){
+module.exports = Phrase;
+
+// Adds 'reverse' to all string.
+String.prototype.reverse = function() {
+    return Array.from(this).reverse().join("");
+}
+
+
+// Defines a Phrase object.
+function Phrase(content) {
+    this.content = content;
+    
+    // Makes the phrase LOUDER.
+    this.louder = function() {
+        return this.content.toUpperCase();
+    }
+    
+    // Returns content processed for palindrome testing.
+    this.processedContent = function processedContent(){
+        return this.letters().toLowerCase();
+    }
+    
+    // Returns the letters in the content.
+    // for eaxample:
+    //   new Phrase("Hello, world!").letters() === "Helloworld"
+    this.letters = function letters() {
+        //return Array.from(this.content).filter( c => c.match(/[a-z]/i) ).join("");
+        return (this.content.match(/[a-z]/gi) || []).join("");
+    }
+    
+    // Returns 'true' for a palindrome, 'false' otherwise.
+    this.palindrome = function palindrome() {
+        return this.processedContent() === this.processedContent().reverse();
+    }
+}
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+    this.content = content;
+    this.translation = translation;
+
+    // Returns translation processed for palindrome testing.
+    this.processedContent = function processedContent(){
+        return this.translation.toLowerCase();
+    }
+}
+
+TranslatedPhrase.prototype = new Phrase();
+
+
+},{}]},{},[1]);
